9b36fec169a9cc927606a930c693725a9b4781de
andrepl <andrepleblanc@gmail.com>
2013-12-02 12:56:16 -0500

    Updated for 1.7.2

--- Plugin/src/main/java/com/norcode/bukkit/scribe/Scribe.java
+++ Plugin/src/main/java/com/norcode/bukkit/scribe/Scribe.java
@@ -1,35 +1,25 @@
 package com.norcode.bukkit.scribe;
 
-import java.lang.reflect.Field;
 import java.util.*;
 import java.util.Map.Entry;
 
 import com.norcode.bukkit.scribe.api.ScribeAnvilInventory;
 
 import net.gravitydevelopment.updater.Updater;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.AnvilInventory;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Scribe extends JavaPlugin implements Listener {
